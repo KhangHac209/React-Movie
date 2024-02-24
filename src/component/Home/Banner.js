c5992d3c3bd97b8311bec91a40f5219a89222a41
LAPTOP-3K4RN8R8\ADMIN <khangvo20p@gmail.com>
2024-02-24 14:32:21 +0700

    fix header

--- src/component/Home/Banner.js
+++ src/component/Home/Banner.js
@@ -9,9 +9,7 @@ const Banner = () => {
             <Carousel.Item>
                 <img src="https://react-film-clone.vercel.app/img/banner_2.png" alt="" />
             </Carousel.Item>
-            <Carousel.Item>
-                <img className="poster" src="https://static1.cbrimages.com/wordpress/wp-content/uploads/2022/11/Spider-Man-Movie-Posters.jpg?q=50&fit=contain&w=1140&h=&dpr=1.5" alt="" />
-            </Carousel.Item>
+            
         </Carousel>
     );
 };
